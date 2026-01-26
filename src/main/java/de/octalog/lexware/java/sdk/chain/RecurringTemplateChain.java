focus-shift/lexoffice-java-sdk

add support for Recurring Templates

src/main/java/de/focus_shift/lexoffice/java/sdk/chain/RecurringTemplateChain.java
@@ -1,0 +1,127 @@
+package de.focus_shift.lexoffice.java.sdk.chain;
+
+import de.focus_shift.lexoffice.java.sdk.RequestContext;
+import de.focus_shift.lexoffice.java.sdk.model.Page;
+import de.focus_shift.lexoffice.java.sdk.model.RecurringTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+@RequiredArgsConstructor
+public class RecurringTemplateChain {
+
+    private final RequestContext context;
+
+    /**
+     * Retrieve a single recurring template by its ID.
+     *
+     * @param id the UUID of the recurring template
+     * @return the RecurringTemplate
+     */
+    public RecurringTemplate get(String id) {
+        return new Get(context).get(id);
+    }
+
+    /**
+     * Returns a Fetch chain for retrieving a paginated list of recurring templates.
+     *
+     * @return Fetch chain for method chaining
+     */
+    public Fetch fetch() {
+        return new Fetch(context);
+    }
+
+    protected static class Get extends ExecutableRequestChain {
+        private static final ParameterizedTypeReference<RecurringTemplate> TYPE_REFERENCE =
+                new ParameterizedTypeReference<>() {};
+
+        public Get(RequestContext context) {
+            super(context, "/recurring-templates");
+        }
+
+        @SneakyThrows
+        public RecurringTemplate get(String id) {
+            getUriBuilder().appendPath("/" + id);
+            return getContext().execute(getUriBuilder(), HttpMethod.GET, TYPE_REFERENCE);
+        }
+    }
+
+    public static class Fetch extends ExecutableRequestChain {
+        private static final ParameterizedTypeReference<Page<RecurringTemplate>> TYPE_REFERENCE =
+                new ParameterizedTypeReference<>() {};
+
+        public Fetch(RequestContext context) {
+            super(context, "/recurring-templates");
+        }
+
+        /**
+         * Pages are zero indexed, thus providing 0 for page will return the first page.
+         */
+        public Fetch page(int page) {
+            super.getUriBuilder()
+                    .addParameter("page", String.valueOf(page));
+            return this;
+        }
+
+        /**
+         * Default page size is set to 25 but can be increased up to 250.
+         */
+        public Fetch pageSize(int pageSize) {
+            super.getUriBuilder()
+                    .addParameter("size", String.valueOf(pageSize));
+            return this;
+        }
+
+        /**
+         * Sort by createdDate in ascending or descending order.
+         */
+        public Fetch sortByCreatedDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("createdDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Sort by updatedDate in ascending or descending order.
+         */
+        public Fetch sortByUpdatedDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("updatedDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Sort by lastExecutionDate in ascending or descending order.
+         */
+        public Fetch sortByLastExecutionDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("lastExecutionDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Sort by nextExecutionDate in ascending or descending order.
+         */
+        public Fetch sortByNextExecutionDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("nextExecutionDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Generic sort method for custom sort parameters.
+         * Format: "property,direction" e.g., "createdDate,ASC"
+         */
+        public Fetch sort(String sort) {
+            super.getUriBuilder()
+                    .addParameter("sort", sort);
+            return this;
+        }
+
+        @SneakyThrows
+        public Page<RecurringTemplate> get() {
+            return getContext().execute(getUriBuilder(), HttpMethod.GET, TYPE_REFERENCE);
+        }
+    }
+}

src/main/java/de/focus_shift/lexoffice/java/sdk/model/ExecutionInterval.java
@@ -1,0 +1,20 @@
+package de.focus_shift.lexoffice.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+public enum ExecutionInterval {
+
+    WEEKLY("WEEKLY"),
+    MONTHLY("MONTHLY"),
+    QUARTERLY("QUARTERLY"),
+    ANNUALLY("ANNUALLY");
+
+    @Getter
+    @JsonValue
+    private String value;
+
+    ExecutionInterval(String value) {
+        this.value = value;
+    }
+}

src/main/java/de/focus_shift/lexoffice/java/sdk/model/ExecutionStatus.java
@@ -1,0 +1,19 @@
+package de.focus_shift.lexoffice.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+public enum ExecutionStatus {
+
+    ACTIVE("ACTIVE"),
+    PAUSED("PAUSED"),
+    ENDED("ENDED");
+
+    @Getter
+    @JsonValue
+    private String value;
+
+    ExecutionStatus(String value) {
+        this.value = value;
+    }
+}

src/main/java/de/focus_shift/lexoffice/java/sdk/model/RecurringTemplate.java
@@ -1,0 +1,74 @@
+package de.focus_shift.lexoffice.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecurringTemplate {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("organizationId")
+    private String organizationId;
+
+    @JsonProperty("createdDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date createdDate;
+
+    @JsonProperty("updatedDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date updatedDate;
+
+    @JsonProperty("version")
+    private Integer version;
+
+    @JsonProperty("language")
+    private String language;
+
+    @JsonProperty("archived")
+    private boolean archived;
+
+    @JsonProperty("address")
+    private Address address;
+
+    @Singular
+    @JsonProperty("lineItems")
+    private List<LineItem> lineItems;
+
+    @JsonProperty("totalPrice")
+    private TotalPrice totalPrice;
+
+    @Singular
+    @JsonProperty("taxAmounts")
+    private List<TaxAmount> taxAmounts;
+
+    @JsonProperty("taxConditions")
+    private TaxConditions taxConditions;
+
+    @JsonProperty("paymentConditions")
+    private PaymentConditions paymentConditions;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("introduction")
+    private String introduction;
+
+    @JsonProperty("remark")
+    private String remark;
+
+    @JsonProperty("recurringTemplateSettings")
+    private RecurringTemplateSettings recurringTemplateSettings;
+}

src/main/java/de/focus_shift/lexoffice/java/sdk/model/RecurringTemplateSettings.java
@@ -1,0 +1,57 @@
+package de.focus_shift.lexoffice.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecurringTemplateSettings {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("startDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date startDate;
+
+    @JsonProperty("endDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date endDate;
+
+    @JsonProperty("finalize")
+    private Boolean finalize;
+
+    @JsonProperty("shippingType")
+    private ShippingType shippingType;
+
+    @JsonProperty("retroactiveInvoice")
+    private Boolean retroactiveInvoice;
+
+    @JsonProperty("executionInterval")
+    private ExecutionInterval executionInterval;
+
+    @JsonProperty("lastExecutionFailed")
+    private Boolean lastExecutionFailed;
+
+    @JsonProperty("lastExecutionErrorMessage")
+    private String lastExecutionErrorMessage;
+
+    @JsonProperty("executionStatus")
+    private ExecutionStatus executionStatus;
+
+    @JsonProperty("lastExecutionDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date lastExecutionDate;
+
+    @JsonProperty("nextExecutionDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date nextExecutionDate;
+}

src/test/java/de/focus_shift/lexoffice/java/sdk/chain/RecurringTemplateChainTest.java
@@ -1,0 +1,54 @@
+package de.focus_shift.lexoffice.java.sdk.chain;
+
+import de.focus_shift.lexoffice.java.sdk.LexofficeApi;
+import de.focus_shift.lexoffice.java.sdk.model.Address;
+import de.focus_shift.lexoffice.java.sdk.model.ExecutionInterval;
+import de.focus_shift.lexoffice.java.sdk.model.ExecutionStatus;
+import de.focus_shift.lexoffice.java.sdk.model.RecurringTemplate;
+import de.focus_shift.lexoffice.java.sdk.model.RecurringTemplateSettings;
+import de.focus_shift.lexoffice.java.sdk.model.ShippingType;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Date;
+
+class RecurringTemplateChainTest {
+
+    @Test
+    @Disabled
+    void exampleUsage() {
+        LexofficeApi lexofficeApi = Mockito.mock(LexofficeApi.class);
+
+        // Example of how the RecurringTemplate model looks
+        RecurringTemplate template = RecurringTemplate.builder()
+                .id("template-uuid")
+                .language("de")
+                .title("Monthly Invoice")
+                .introduction("Monthly recurring invoice")
+                .remark("Thank you for your business")
+                .address(Address.builder()
+                        .contactId("contact-uuid")
+                        .build())
+                .recurringTemplateSettings(RecurringTemplateSettings.builder()
+                        .startDate(new Date())
+                        .endDate(new Date())
+                        .finalize(true)
+                        .shippingType(ShippingType.SERVICE)
+                        .executionInterval(ExecutionInterval.MONTHLY)
+                        .executionStatus(ExecutionStatus.ACTIVE)
+                        .build())
+                .build();
+
+        // GET single template
+        lexofficeApi.recurringTemplates().get("template-uuid");
+
+        // GET paginated list with sorting
+        lexofficeApi.recurringTemplates()
+                .fetch()
+                .page(0)
+                .pageSize(50)
+                .sortByNextExecutionDate(true)
+                .get();
+    }
+}

octalog-de/lexware-java-sdk

(cherry picked from commit 4197b90de8f468dc7fc1a7739553fe88276156b0)

src/main/java/de/octalog/lexware/java/sdk/chain/RecurringTemplateChain.java
@@ -1,0 +1,127 @@
+package de.octalog.lexware.java.sdk.chain;
+
+import de.octalog.lexware.java.sdk.RequestContext;
+import de.octalog.lexware.java.sdk.model.Page;
+import de.octalog.lexware.java.sdk.model.RecurringTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+@RequiredArgsConstructor
+public class RecurringTemplateChain {
+
+    private final RequestContext context;
+
+    /**
+     * Retrieve a single recurring template by its ID.
+     *
+     * @param id the UUID of the recurring template
+     * @return the RecurringTemplate
+     */
+    public RecurringTemplate get(String id) {
+        return new Get(context).get(id);
+    }
+
+    /**
+     * Returns a Fetch chain for retrieving a paginated list of recurring templates.
+     *
+     * @return Fetch chain for method chaining
+     */
+    public Fetch fetch() {
+        return new Fetch(context);
+    }
+
+    protected static class Get extends ExecutableRequestChain {
+        private static final ParameterizedTypeReference<RecurringTemplate> TYPE_REFERENCE =
+                new ParameterizedTypeReference<>() {};
+
+        public Get(RequestContext context) {
+            super(context, "/recurring-templates");
+        }
+
+        @SneakyThrows
+        public RecurringTemplate get(String id) {
+            getUriBuilder().appendPath("/" + id);
+            return getContext().execute(getUriBuilder(), HttpMethod.GET, TYPE_REFERENCE);
+        }
+    }
+
+    public static class Fetch extends ExecutableRequestChain {
+        private static final ParameterizedTypeReference<Page<RecurringTemplate>> TYPE_REFERENCE =
+                new ParameterizedTypeReference<>() {};
+
+        public Fetch(RequestContext context) {
+            super(context, "/recurring-templates");
+        }
+
+        /**
+         * Pages are zero indexed, thus providing 0 for page will return the first page.
+         */
+        public Fetch page(int page) {
+            super.getUriBuilder()
+                    .addParameter("page", String.valueOf(page));
+            return this;
+        }
+
+        /**
+         * Default page size is set to 25 but can be increased up to 250.
+         */
+        public Fetch pageSize(int pageSize) {
+            super.getUriBuilder()
+                    .addParameter("size", String.valueOf(pageSize));
+            return this;
+        }
+
+        /**
+         * Sort by createdDate in ascending or descending order.
+         */
+        public Fetch sortByCreatedDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("createdDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Sort by updatedDate in ascending or descending order.
+         */
+        public Fetch sortByUpdatedDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("updatedDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Sort by lastExecutionDate in ascending or descending order.
+         */
+        public Fetch sortByLastExecutionDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("lastExecutionDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Sort by nextExecutionDate in ascending or descending order.
+         */
+        public Fetch sortByNextExecutionDate(boolean asc) {
+            super.getUriBuilder()
+                    .addParameter("sort", String.format("nextExecutionDate,%s", asc ? "ASC" : "DESC"));
+            return this;
+        }
+
+        /**
+         * Generic sort method for custom sort parameters.
+         * Format: "property,direction" e.g., "createdDate,ASC"
+         */
+        public Fetch sort(String sort) {
+            super.getUriBuilder()
+                    .addParameter("sort", sort);
+            return this;
+        }
+
+        @SneakyThrows
+        public Page<RecurringTemplate> get() {
+            return getContext().execute(getUriBuilder(), HttpMethod.GET, TYPE_REFERENCE);
+        }
+    }
+}

src/main/java/de/octalog/lexware/java/sdk/model/ExecutionInterval.java
@@ -1,0 +1,20 @@
+package de.octalog.lexware.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+public enum ExecutionInterval {
+
+    WEEKLY("WEEKLY"),
+    MONTHLY("MONTHLY"),
+    QUARTERLY("QUARTERLY"),
+    ANNUALLY("ANNUALLY");
+
+    @Getter
+    @JsonValue
+    private String value;
+
+    ExecutionInterval(String value) {
+        this.value = value;
+    }
+}

src/main/java/de/octalog/lexware/java/sdk/model/ExecutionStatus.java
@@ -1,0 +1,19 @@
+package de.octalog.lexware.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+public enum ExecutionStatus {
+
+    ACTIVE("ACTIVE"),
+    PAUSED("PAUSED"),
+    ENDED("ENDED");
+
+    @Getter
+    @JsonValue
+    private String value;
+
+    ExecutionStatus(String value) {
+        this.value = value;
+    }
+}

src/main/java/de/octalog/lexware/java/sdk/model/RecurringTemplate.java
@@ -1,0 +1,74 @@
+package de.octalog.lexware.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecurringTemplate {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("organizationId")
+    private String organizationId;
+
+    @JsonProperty("createdDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date createdDate;
+
+    @JsonProperty("updatedDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date updatedDate;
+
+    @JsonProperty("version")
+    private Integer version;
+
+    @JsonProperty("language")
+    private String language;
+
+    @JsonProperty("archived")
+    private boolean archived;
+
+    @JsonProperty("address")
+    private Address address;
+
+    @Singular
+    @JsonProperty("lineItems")
+    private List<LineItem> lineItems;
+
+    @JsonProperty("totalPrice")
+    private TotalPrice totalPrice;
+
+    @Singular
+    @JsonProperty("taxAmounts")
+    private List<TaxAmount> taxAmounts;
+
+    @JsonProperty("taxConditions")
+    private TaxConditions taxConditions;
+
+    @JsonProperty("paymentConditions")
+    private PaymentConditions paymentConditions;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("introduction")
+    private String introduction;
+
+    @JsonProperty("remark")
+    private String remark;
+
+    @JsonProperty("recurringTemplateSettings")
+    private RecurringTemplateSettings recurringTemplateSettings;
+}

src/main/java/de/octalog/lexware/java/sdk/model/RecurringTemplateSettings.java
@@ -1,0 +1,57 @@
+package de.octalog.lexware.java.sdk.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecurringTemplateSettings {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("startDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date startDate;
+
+    @JsonProperty("endDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date endDate;
+
+    @JsonProperty("finalize")
+    private Boolean finalize;
+
+    @JsonProperty("shippingType")
+    private ShippingType shippingType;
+
+    @JsonProperty("retroactiveInvoice")
+    private Boolean retroactiveInvoice;
+
+    @JsonProperty("executionInterval")
+    private ExecutionInterval executionInterval;
+
+    @JsonProperty("lastExecutionFailed")
+    private Boolean lastExecutionFailed;
+
+    @JsonProperty("lastExecutionErrorMessage")
+    private String lastExecutionErrorMessage;
+
+    @JsonProperty("executionStatus")
+    private ExecutionStatus executionStatus;
+
+    @JsonProperty("lastExecutionDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date lastExecutionDate;
+
+    @JsonProperty("nextExecutionDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date nextExecutionDate;
+}

src/test/java/de/octalog/lexware/java/sdk/chain/RecurringTemplateChainTest.java
@@ -1,0 +1,54 @@
+package de.octalog.lexware.java.sdk.chain;
+
+import de.octalog.lexware.java.sdk.LexwareApi;
+import de.octalog.lexware.java.sdk.model.Address;
+import de.octalog.lexware.java.sdk.model.ExecutionInterval;
+import de.octalog.lexware.java.sdk.model.ExecutionStatus;
+import de.octalog.lexware.java.sdk.model.RecurringTemplate;
+import de.octalog.lexware.java.sdk.model.RecurringTemplateSettings;
+import de.octalog.lexware.java.sdk.model.ShippingType;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Date;
+
+class RecurringTemplateChainTest {
+
+    @Test
+    @Disabled
+    void exampleUsage() {
+        LexwareApi lexwareApi = Mockito.mock(LexwareApi.class);
+
+        // Example of how the RecurringTemplate model looks
+        RecurringTemplate template = RecurringTemplate.builder()
+                .id("template-uuid")
+                .language("de")
+                .title("Monthly Invoice")
+                .introduction("Monthly recurring invoice")
+                .remark("Thank you for your business")
+                .address(Address.builder()
+                        .contactId("contact-uuid")
+                        .build())
+                .recurringTemplateSettings(RecurringTemplateSettings.builder()
+                        .startDate(new Date())
+                        .endDate(new Date())
+                        .finalize(true)
+                        .shippingType(ShippingType.SERVICE)
+                        .executionInterval(ExecutionInterval.MONTHLY)
+                        .executionStatus(ExecutionStatus.ACTIVE)
+                        .build())
+                .build();
+
+        // GET single template
+        lexwareApi.recurringTemplates().get("template-uuid");
+
+        // GET paginated list with sorting
+        lexwareApi.recurringTemplates()
+                .fetch()
+                .page(0)
+                .pageSize(50)
+                .sortByNextExecutionDate(true)
+                .get();
+    }
+}